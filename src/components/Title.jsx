f9f714c1181fe69f502e4d89ada40182fa48ed6c
kirboy <david.caudill@live.de>
2021-03-22 20:57:49 +0100

    Updates

--- src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-import { Typography } from "@material-ui/core";
-import { withTheme } from '@material-ui/core/styles';
-
-function Title(props) {
-  return <Typography variant="h3" style={{textDecoration: 'underline', textDecorationColor: props.theme.palette.primary.main, paddingBottom: 32}}>{props.title}</Typography>;
-}
-
-export default withTheme(Title);
